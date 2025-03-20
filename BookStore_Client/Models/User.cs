ea767c16aa5d710e3b70afec8a4b477b629464f7
Lê Nhật Trường - N1 <truonglee2605@gmail.com>
2025-03-20 15:13:58 +0700

    add models client
    add models client

---
+++ BookStore_Client/Models/User.cs
@@ -0,0 +1,36 @@
+﻿namespace BookStore_Client.Models;
+
+public partial class User
+{
+    public int UserID { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? FullName { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public DateOnly? CreateAt { get; set; }
+
+    public DateOnly? UpdateAt { get; set; }
+
+    public bool? IsDelete { get; set; }
+
+    public int? Role { get; set; }
+
+    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<WishList> WishLists { get; set; } = new List<WishList>();
+}
---
+++ BookStore_Client/Models/WishList.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore_Client.Models;
+
+public partial class WishList
+{
+    [Key]
+    public int WishID { get; set; }
+
+    public int? BookID { get; set; }
+
+    public int? UserID { get; set; }
+
+    public virtual Book? Book { get; set; }
+
+    public virtual User? User { get; set; }
+}
