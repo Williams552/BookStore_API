99b78a9dd7bd0fac55c4c92faa5a9d238f3ab772
NguyenKhang09 <Khang260709@gmail.com>
2025-03-20 13:10:19 +0700

    Thêm giao diện Client

---
+++ BookStore_Client/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_Client.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
